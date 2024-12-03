10dba464e0a90c4fc17e44bf4781735b4c02f64f
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-03 18:48:39 +0600

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const age = people.map((person) => {
+        if (person.yearOfDeath === undefined) {
+            return 2024 - person.yearOfBirth;
+        } else {
+        return person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    const oldest = Math.max(...age);
+    const index = age.indexOf(oldest);
+    return people[index];
 };
 
 // Do not edit below this line
