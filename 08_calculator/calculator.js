1e290fe1507607215b2530b40f9280efd944f920
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-03 15:19:05 +0600

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => { return sum + curr}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => {
+    return total * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  } else {
+    let fact = 1;
+    for(let i = 1; i <= a; i++) {
+      fact *= i;
+    } return fact;
+  }
 };
 
 // Do not edit below this line
