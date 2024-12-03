70e5772de540fe02576487efcdee1d082c3cb112
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-03 16:25:16 +0600

    Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const aphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((ch) => aphanumerical.includes(ch))
+    .join('');
+    const revCleanedStr = cleanedStr.split('').reverse().join('');
 
+    return revCleanedStr === cleanedStr;
 };
 
 // Do not edit below this line
